4a7c57c6778c9a0b7db653aac2dcca4188047613
joelperry93 <joelperry@me.com>
2015-02-07 20:06:17 +0000

    Created create_subreddits_table migration

---
+++ database/migrations/2015_02_07_195049_create_subreddits_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubredditsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('subreddits', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->text('description');
+			$table->boolean('adult_content');
+			$table->timestamps();
+			$table->integer('creator_id')->unsigned();
+			$table->boolean('deleted')->default(false);
+			$table->foreign('creator_id')->references('id')->on('users');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('subreddits');
+	}
+
+}
