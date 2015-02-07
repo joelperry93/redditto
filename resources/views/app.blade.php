11e050f9048ab15ec879d3b7466cb8ac716420e3
Joel Perry <vagrant@homestead>
2015-02-07 19:33:33 +0000

    Replaced 'Laravel' titles with 'redditto'

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Laravel</title>
+	<title>redditto | a clone of the front page of the internet</title>
 
 	<link href="/css/app.css" rel="stylesheet">
 
@@ -28,13 +28,13 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">Laravel</a>
+				<a class="navbar-brand" href="#">redditto</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">
+				<!--<ul class="nav navbar-nav">
 					<li><a href="/">Home</a></li>
-				</ul>
+				</ul>-->
 
 				<ul class="nav navbar-nav navbar-right">
 					@if (Auth::guest())
